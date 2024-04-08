76b40121c9821c2e2ccf55b6fb353ae7de7de1dd
alexquar <alexanderquarrie@gmail.com>
2024-04-08 11:32:21 -0400

    loading...

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,10 +1,14 @@
-import TicketLIst from "./TicketLIst";
+import { Suspense } from "react";
+import TicketList from "./TicketLIst";
+import Loading from "../loading";
 
 export default function Tickets() {
   return (
     <div>
       <h2>Tickets</h2>
-      <TicketLIst />
+      <Suspense fallback={<Loading />}>
+      <TicketList />
+      </Suspense>
     </div>
   )
 }
