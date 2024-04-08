48412b9a9c22567d1d1bfbf88531bc6ccbbb8353
alexquar <alexanderquarrie@gmail.com>
2024-04-08 17:17:57 -0400

    signup and login pages

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,8 +1,15 @@
+"use client"
+
+import AuthForm from "../AuthForm";
 
 export default function Login() {
+  const handleSubmit = (e,email,password) => {
+    e.preventDefault()
+  }
   return (
     <main>
         <h2 className="text-center">Login</h2>
+        <AuthForm handleSubmit={handleSubmit} />
     </main>
   )
 }
--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,7 +1,15 @@
+"use client"
+import AuthForm from "../AuthForm";
+
+
 export default function Signup() {
+  const handleSubmit = (e,email,password) => {
+    e.preventDefault()
+  }
   return (
     <main>
         <h2 className="text-center">Signup</h2>
+        <AuthForm handleSubmit={handleSubmit}/>
     </main>
   )
 }
