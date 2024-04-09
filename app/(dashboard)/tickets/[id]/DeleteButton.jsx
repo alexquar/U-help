f589c0ad6cf61b873c01d776f4b92eefc423147f
alexquar <alexanderquarrie@gmail.com>
2024-04-09 13:30:12 -0400

    deleting

--- app/(dashboard)/tickets/[id]/DeleteButton.jsx
+++ app/(dashboard)/tickets/[id]/DeleteButton.jsx
@@ -1,11 +1,23 @@
 'use client'
 import { useState } from "react"
 import {TiDelete} from 'react-icons/ti'
+import { useRouter } from "next/navigation"
 export default function DeleteButton({id}) {
     const [isPending, setIsPending]= useState(false)
+    const router = useRouter()
     const handleClick = async () => {
         setIsPending(true)
-
+        const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
+            method : 'DELETE'
+        })    
+        const json = await res.json()
+        if(json.error){
+            console/log(json.error.message)
+            setIsPending(false)
+        } else {
+            router.refresh()
+            router.push('/tickets')
+        }
     }
   return (
     <button className="btn-primary"
--- app/api/tickets/[id]/route.js
+++ app/api/tickets/[id]/route.js
@@ -1,19 +1,11 @@
 
-import { NextResponse } from "next/server"
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
 
-export const dynamic = 'force-dynamic'
-
-export async function GET(_, {params}){
-    const {id} = params
-    const res = await fetch("http://localhost:4000/tickets/"+id)
-    const ticket = await res.json()
-      if (!res.ok) {
-        return NextResponse.json({error:'Cannot find the ticket...'},{
-            status:404
-        })
-      }
-
-      return NextResponse.json(ticket, {
-        status: 200
-      })
+export async function DELETE(_, {params}){
+  const {id} = params
+  const supabase = createRouteHandlerClient({ cookies })
+  const {error}=supabase.from('tickets').delete().eq('id',id)
+  return NextResponse.json({error})
 }
